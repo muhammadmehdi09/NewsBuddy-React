6f733ce3c18c95bf64c10c454fd9a0b045cd98b3
Coding Verse <muhammadmehdi09@gmail.com>
2025-12-02 17:46:33 +0500

    Connecting with api

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,43 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articles: []
+    };
+  }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=7f26e1590bd540e7af9d4c233412ed23";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
+  }
+
   render() {
+    let items = [];
+
+    for (let i = 0; i < this.state.articles.length; i++) {
+      let article = this.state.articles[i];
+      let desc = article.description
+      if (desc !== null && desc !== undefined) {
+        if (desc.split("").length > 100) {
+          desc = desc.split("").splice(0, 97).join("") + "..."
+        }
+      }
+      items.push(<NewsItem title={article.title} desc={desc} imgUrl={article.urlToImage} newsUrl={article.url} />);
+    }
+
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-6 place-items-center">
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        {items}
       </div>
-    )
+    );
   }
+
 }
 
 export default News
+
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
-import img from '../assets/image.jpg'
+import fallbackImg from '../assets/fallback.jpg';
 
 export class NewsItem extends Component {
   render() {
+    let { title, desc, imgUrl, newsUrl } = this.props
     return (
-      <div className="w-72 bg-white shadow rounded overflow-hidden">
-        <img src={img} alt="..." className="w-full h-40 object-cover" />
-        <div className="p-4">
-          <h5 className="text-lg font-semibold mb-2">Card title</h5>
-          <p className="text-gray-600 mb-4">
-            Some quick example text to build on the card title and make up the bulk of the card’s content.
-          </p>
-          <a href="#" className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
-            Go somewhere
-          </a>
+      <div className="h-[550px]">
+        <div className="w-72 bg-white shadow rounded overflow-hidden min-h-[400px]">
+          <img src={imgUrl || fallbackImg} alt="" className="w-full h-40 object-cover" onError={(e) => (e.target.src = fallbackImg)} />
+          <div className="p-4">
+            <h5 className="text-lg font-semibold mb-2">{title}</h5>
+            <p className="text-gray-600 mb-4">
+              {desc}
+            </p>
+            <a href={newsUrl} className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+              Read More
+            </a>
+          </div>
         </div>
       </div>
     )
